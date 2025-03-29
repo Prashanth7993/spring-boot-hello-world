39ada495a95bd962db132116729b6d77b3076f7b
Prashanth7993 <jprashanth429@gmail.com>
2025-03-29 06:53:15 +0000

    Updated controller

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, World! changes made after deploy";
+        return "Hello, World! changes made deploy";
     }
 }
